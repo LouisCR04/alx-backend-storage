14a073ed71d3df32ab169770d5c1f424a8890a24
Louis#04 <louiskariuki04@gmail.com>
2024-06-21 07:38:33 +0000

    Get functions + counter decorator funtion added

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,19 @@ from typing import Union, Optional, Callable
 from uuid import uuid4, UUID
 
 
+def count_calls(method: Callable) -> Callable:
+    """ Decorator that counts how much methods of Cache class are called """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper for decorator functionality """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
 class Cache:
     """ Implements a cache class """
     def __init__(self):
@@ -14,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random key, stores the input data in redis & returns key
@@ -22,3 +36,26 @@ class Cache:
         self._redis.set(random_key, data)
 
         return random_key
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """ Gets data from redis & converts it back to desired format """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Parametrizes obtained value from redis into string """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parametrizes obtained value from redis into an integer """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
